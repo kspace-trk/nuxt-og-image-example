b0a43aadfff038459f703beab3d09e0cdd9b9dde
kspace-trk <keigonakao.0825@gmail.com>
2024-02-15 20:17:00 +0900

    add site.url to nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['nuxt-og-image'],
+  site: {
+  // production URL
+  url: 'https://nuxt-og-image-example.vercel.app/',
+  },
 })
