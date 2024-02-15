fd2295bd2c72f2947ca4ee008e73df774ab8cc90
kspace-trk <keigonakao.0825@gmail.com>
2024-02-15 20:33:01 +0900

    add: OG Image component configuration

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+defineOgImageComponent('NuxtSeo', {
+  title: 'Hello OG Image 👋',
+  description: 'Look what at me in dark mode',
+  theme: '#ff0000',
+  colorMode: 'dark',
+})
+</script>
 <template>
   <div>
     <NuxtWelcome />
