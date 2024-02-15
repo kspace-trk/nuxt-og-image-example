83eccdfbdda53cac348becf4ebd4195107b25e77
kspace-trk <keigonakao.0825@gmail.com>
2024-02-15 20:52:01 +0900

    add: OG metadata to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,4 +6,18 @@ export default defineNuxtConfig({
   // production URL
   url: 'https://nuxt-og-image-example.vercel.app/',
   },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'ja', prefix: 'og: https://nuxt-og-image-example.vercel.app/'
+      },
+      title: 'Nuxt OG Image Example',
+      meta: [
+        { hid: 'og:type',  property: 'og:type', content: 'website' },
+        { hid: 'og:site_name', property: 'og:site_name', content: 'Nuxt OG Image Example' },
+        { hid: 'og:url',  property: 'og:url', content: 'https://nuxt-og-image-example.vercel.app/' },
+        { hid: 'og:title', property: 'og:title', content: 'Nuxt OG Image Example' },
+      ]
+    }
+  },
 })
